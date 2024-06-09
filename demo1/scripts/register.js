e47264eedee4b74b7a319646e9f977ed5b9109db
HoshenP <hoshen.perez@gmail.com>
2024-06-09 21:25:17 +0300

    Save image in localStorage and pull it after to display

--- demo1/scripts/register.js
+++ demo1/scripts/register.js
@@ -3,19 +3,72 @@ import { User } from '../scripts/utils.js';
 $(document).ready( () => {
     
     $("#registerBtn").click(()=> {
-        // console.log($("#username").val());
-        // console.log($("#profilePicture").prop('files')[0]);
+        console.log($("#profilePicture").prop('files')[0]);
 
         if ($("#username").val() == "" || $("#password").val() == "" || !$("#profilePicture").prop('files')[0]){
             console.log("some info is missing..");
         }
         else{
             console.log("all is filled");
+
+             // picture must be below 5MB in order to save in local storage
+            let fileReader = new FileReader();
+            fileReader.onload = function () {
+                let image = fileReader.result;  
+
+                if (JSON.parse(localStorage.getItem("myUsers"))) {
+                    let tempUsers = JSON.parse(localStorage.getItem("myUsers"));
+                    let newUser = new User(
+                        $("#username").val(),
+                        $("#password").val(),
+                        image);
+        
+                    tempUsers.push(newUser);
+                    localStorage.setItem("myUsers", JSON.stringify(tempUsers));
+
+                } else {
+                    let tempUsers= [];
+                    let newUser = new User(
+                        $("#username").val(),
+                        $("#password").val(),
+                        image);
+        
+                    tempUsers.push(newUser);
+                    localStorage.setItem("myUsers", JSON.stringify(tempUsers));
+                }
+                
+            };
+
+            fileReader.readAsDataURL($('#profilePicture').prop('files')[0]);
+
+            // get image from localStorage and display test
+            document.getElementById("displayImage").style.backgroundImage = `url(${JSON.parse(localStorage.getItem("myUsers"))[0].profilePicture})`;
+
+
+            
+
+
+
+
+
+
         }
-
-
-
     });
+
+        // how to use file reader :)
+        // $('#profilePicture').on('change', function () {
+        //     console.log("change detected");
+
+        //     let fileReader = new FileReader();
+        //     fileReader.onload = function () {
+        //       let data = fileReader.result;  // data <-- in this var you have the file data in Base64 format
+        //       document.getElementById("displayImage").style.backgroundImage = `url(${data})`;
+        //     };
+        //     fileReader.readAsDataURL($('#profilePicture').prop('files')[0]);
+
+        //     console.log("changes should be made..");
+        // });
+        
 
 
 });
