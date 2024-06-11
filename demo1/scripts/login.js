617293f0d8c4504d5402358ebf4104f7356372e3
HoshenP <hoshen.perez@gmail.com>
2024-06-11 18:32:13 +0300

    done

--- demo1/scripts/login.js
+++ demo1/scripts/login.js
@@ -10,16 +10,19 @@ $(document).ready( () => {
         }
         else{
             if (JSON.parse(localStorage.getItem("myUsers"))) {
+                let foundUser = false;
                 let myUsers = JSON.parse(localStorage.getItem("myUsers"));
                 for (let x in myUsers) {
                     if (myUsers[x].email == $("#loginEmail").val() && myUsers[x].password == $("#loginPassword").val()) {
                         localStorage.setItem("currentLoggedUser", JSON.stringify(myUsers[x]));
+                        foundUser = true;
                         window.location.href = "index.html";
                     }
                 }
+                if (!foundUser){
+                    alert("Wrong email or password");
+                }
             }
-
-    
 
         }
     });
